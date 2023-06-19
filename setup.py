8c6912ca4e7a4c4bc76055eff92c18d9a383db85
GeneralYadoc <133023047+GeneralYadoc@users.noreply.github.com>
2023-06-19 23:01:11 +0900

    Update ChatAIAgent to v1.1.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def _requires_from_file(filename):
 
 setup(
     name="chatai-stream",
-    version="1.0.1",
+    version="1.0.2",
     license="MIT",
     description="ChatGPT reacts YouTube chat messages.",
     author="General Yadoc",
